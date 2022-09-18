034038e943bafd1869608808364a9f04dc375895
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-09-18 17:30:46 +0200

    ippower

--- website/public/assets/js/scripts.js
+++ website/public/assets/js/scripts.js
@@ -64,7 +64,7 @@ if (document.getElementsByClassName('ram')) {
 
             setInterval(() => {
                 serverJson(item.firstChild.id);
-            }, 20000);
+            }, 5000);
 
         });
     });
