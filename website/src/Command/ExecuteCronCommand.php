da4dea067686daf240b66c89ee519bbba4d3c9e7
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2024-04-22 19:20:54 +0200

    cron commande

--- website/src/Command/ExecuteCronCommand.php
+++ website/src/Command/ExecuteCronCommand.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Command;
 
 use App\Controller\CronController;
-use App\Library\IppowerLibrary;
 use App\Repository\RestartRepository;
 use App\Repository\ServerRepository;
+use App\Library\IppowerLibrary;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,36 +12,36 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ExecuteCronCommand extends Command
 {
-    protected static $defaultName = 'app:execute-cron';
+protected static $defaultName = 'app:execute-cron';
 
-    private RestartRepository $restartRepository;
-    private IppowerLibrary $ippowerLibrary;
-    private EntityManagerInterface $entityManager;
-    private ServerRepository $serverRepository;
+private $restartRepository;
+private $ippowerLibrary;
+private $entityManager;
+private $serverRepository;
 
-    public function __construct(RestartRepository $restartRepository, IppowerLibrary $ippowerLibrary, EntityManagerInterface $entityManager, ServerRepository $serverRepository)
-    {
-        parent::__construct();
-        $this->restartRepository = $restartRepository;
-        $this->ippowerLibrary = $ippowerLibrary;
-        $this->entityManager = $entityManager;
-        $this->serverRepository = $serverRepository;
-    }
+public function __construct(RestartRepository $restartRepository, IppowerLibrary $ippowerLibrary, EntityManagerInterface $entityManager, ServerRepository $serverRepository)
+{
+parent::__construct();
+$this->restartRepository = $restartRepository;
+$this->ippowerLibrary = $ippowerLibrary;
+$this->entityManager = $entityManager;
+$this->serverRepository = $serverRepository;
+}
 
-    protected function configure(): void
-    {
-        $this->setDescription('Execute the cron job');
-    }
+protected function configure(): void
+{
+$this->setDescription('Execute the cron job');
+}
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
+protected function execute(InputInterface $input, OutputInterface $output): int
+{
 // Appel de la méthode index du CronController avec les dépendances injectées
-        $cronController = new CronController($this->restartRepository, $this->ippowerLibrary, $this->entityManager, $this->serverRepository);
-        $response = $cronController->index();
+$cronController = new CronController($this->restartRepository, $this->ippowerLibrary, $this->entityManager, $this->serverRepository);
+$response = $cronController->index();
 
 // Gestion de la réponse
-        $output->writeln($response);
+$output->writeln($response);
 
-        return Command::SUCCESS;
-    }
+return Command::SUCCESS;
+}
 }
